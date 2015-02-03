f27ebaca4ba4de9b33d435c6516521ba65f4b0fd
Jason Diamond <jason@diamond.name>
2015-02-03 13:25:00 -0800

    angular

---
+++ 05_AngularIntro/WebApplication1/WebApplication1/app.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="angular.min.js" />
+
+(function (angular) {
+
+    var mod = angular.module("myApp", []);
+
+    mod.controller("PersonCtrl", function ($scope) {
+        $scope.acceptTerms = "yes";
+        $scope.person = {
+            name: "Jason",
+            age: 23,
+            children: [
+                { name: "Mira" },
+                { name: "Joe" },
+                { name: "Fred" },
+                { name: "Tess" }
+            ]
+        };
+
+        $scope.removeKid = function (kid) {
+            var idx = $scope.person.children.indexOf(kid);
+            console.log(idx);
+            $scope.person.children.splice(idx, 1);
+        }
+
+        $scope.canEditName = function (person) {
+            return person.age > 0 && person.name !== "Brock";
+        }
+
+        $scope.addKid = function (name) {
+            $scope.person.children.push({ name: name });
+        }
+
+        $scope.update = function (person, accept) {
+            console.log("person was updated!", person, accept);
+        }
+    });
+
+})(angular);

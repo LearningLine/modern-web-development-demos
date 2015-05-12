4d69ed35a205f2c8d856f1a0b49255d2ddb1c492
Maurice de Beijer <mauricedb@computer.org>
2015-05-12 12:41:53 +0100

    04 - jQuery UI

--- Mod04/wwwroot/demo.js
+++ Mod04/wwwroot/demo.js
@@ -6,8 +6,44 @@
 $(function () {
     'use strict';
 
-    
 
+    //$('#large').click(function(){
+    //    //$(this).hide('shake');
+    //    //$(this).animate({backgroundColor:  'red'});
+    //    //$(this).switchClass('large', 'small', 1000, 'easeInOutBounce');
+    //    $(this).switchClass('large', 'small', 1000, 'easeInOutCubic');
+    //
+    //})
+
+    //
+    //$('#small').draggable({
+    //    stop: function(){
+    //        console.log("Stopping the drag action")
+    //    }
+    //
+    //});
+    //
+    //
+    //$('#large').droppable({
+    //
+    //    drop: function(e, ui){
+    //        console.log('Something was dropped', e, ui)
+    //    }
+    //});
+
+
+    //$('#large').resizable();
+    //
+    //
+    //
+    $.fn.makeRed = function(){
+        this.css('backgroundColor', 'red');
+
+        return this;
+    };
+
+
+    $('#large').makeRed().text('Some text');
 });
 
 

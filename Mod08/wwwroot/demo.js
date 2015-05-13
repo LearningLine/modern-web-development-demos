eec05920949fb6e5424ddc8b8fc94cc9b95cc2ca
Maurice de Beijer <mauricedb@computer.org>
2015-05-13 16:01:20 +0100

    08 - AngularJS Directives

--- Mod08/wwwroot/demo.js
+++ Mod08/wwwroot/demo.js
@@ -4,9 +4,45 @@
 
 (function () {
 
-    var app = angular.module('movieApp', []);
+    var app = angular.module('demoApp', []);
 
+    app.controller('dummyController', function($scope){
+$scope.$watch('firstName', function (newVal, oldValue){
+        console.log(oldValue, newVal);
+    }
+)
+    });
 
+    app.directive('setFocus', function(){
+        return {
+            link: function(scope, element){
+
+                element[0].focus();
+
+            }
+        }
+    });
+
+    app.directive('addTemplate', function($rootScope){
+        return {
+            //template: '<span>The template code</span>'
+            templateUrl: '/addTemplate.html',
+            transclude: true,
+            scope:true,
+            link: function(scope){
+
+                //console.log(scope.$id, scope.$parent, scope.$root, $rootScope)
+                console.log(scope.$id, scope.$parent.$id)
+
+            }
+        };
+    })
+
+    app.directive('isoScope', function(){
+        return {
+            scope: {}
+        };
+    })
 
 })();
 

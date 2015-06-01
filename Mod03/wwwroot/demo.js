98342f7ebd78ffad3700b8779db8ebd3a1e93112
Maurice de Beijer <mauricedb@computer.org>
2015-06-01 17:37:34 +0100

    End of part one

--- Mod03/wwwroot/demo.js
+++ Mod03/wwwroot/demo.js
@@ -3,3 +3,31 @@
  */
 
 
+$(function(){
+    console.log('The document is loaded and ready');
+
+
+    //$('li').css('color', 'red');
+    //$('li.myStyle').css('color', 'red');
+    //$('ul .myStyle').css('color', 'red');
+    //$('#8').css('color', 'red');
+    //$('#notThere').css('color', 'red');
+
+    console.log($('ul .myStyle').length)
+    console.log($('#8').length)
+    console.log($('#notThere').length)
+
+    //$('li:first').css('color', 'red');
+    //$('li:even').css('color', 'red');
+    $('li:nth-child(even)').css('color', 'red');
+
+    $('ul li .cool')
+
+
+
+
+
+
+
+
+});

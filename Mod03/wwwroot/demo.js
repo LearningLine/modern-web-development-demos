c7328bd4a9650b5209da00d9a3afba6274c218c1
Maurice de Beijer <mauricedb@computer.org>
2015-06-02 11:00:48 +0100

    03 - jQuery

--- Mod03/wwwroot/demo.js
+++ Mod03/wwwroot/demo.js
@@ -19,15 +19,62 @@ $(function(){
 
     //$('li:first').css('color', 'red');
     //$('li:even').css('color', 'red');
-    $('li:nth-child(even)').css('color', 'red');
+    //$('li:nth-child(even)').css('color', 'red');
 
-    $('ul li .cool')
+    //$('ul li .cool')
+    //
+    //
+    //
+    //var firstSelector = $('ul');
+    //$('.myStyle',  firstSelector).css('color', 'red');
 
 
 
+    //$('#btn').click(function(){
+    $('#btn').bind('click', function(){
+        //$('li').toggle();
+        //$('li').slideToggle(10000);
+        //$('ul').slideToggle(10000);
+        //$('ul').fadeToggle(5000);
+        //var opacity = 1;
+        //setInterval(function(){
+        //    opacity -= 0.1;
+        //    $('ul').css('opacity', opacity)
+        //}, 100)
+        //$('ul').animate({
+        //    opacity: 0.25,
+        //    left: "+=50"
+        //    //height: "toggle"
+        //}, 5000)
+
+
+        //$('li').text('Some other text').css('color','red').attr('foo', 12)
+
+    //$('<li>').text('new element').appendTo('ul')
+    //$('<li>Some text</li>').appendTo('ul')
+       $('li:eq(5)').before( $('<li id="2">Some text</li>'));
+    });
 
 
 
+    $('#btnLoad').click(function(){
 
+        $('#loaded').load('/subPage.html div.content')
+    })
 
+    $('#loadJSON').click(function(){
+
+        $.getJSON('/api/movies').then(function(movies){
+            console.table(movies)
+
+            movies.forEach(function(movie){
+                $('<li>')
+                    .attr('id', movie.id)
+                    .text(movie.title)
+                    .appendTo('#movies')
+
+            });
+        });
+
+    })
 });

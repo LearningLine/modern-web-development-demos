21da9937ad7120e738bc5e3c4ac717084730725b
Maurice de Beijer <mauricedb@computer.org>
2015-06-02 14:21:25 +0100

    04 - jQuery UI

--- Mod04/wwwroot/demo.js
+++ Mod04/wwwroot/demo.js
@@ -1,3 +1,29 @@
 /**
  * Created by Maurice on 6/2/2015.
  */
+
+
+$(function(){
+
+    $('#two').click(function(){
+        //$(this).hide('shake');
+        //$(this).effect('shake').effect('explode');
+        //$(this).effect('highlight');
+
+        //$(this).animate({backgroundColor: 'red'})
+    //$(this).switchClass('blue', 'red', 2000)
+    //$(this).switchClass('blue', 'red', 2000, 'easeInOutCubic')
+    //$(this).switchClass('blue', 'red', 2000, 'easeInOutElastic')
+
+    })
+    //
+    //$('#one').draggable();
+    //
+    //$('#two').droppable({
+    //    drop: function(){
+    //        console.log('SOmething was dropped')
+    //    }
+    //});
+
+    $('#two').resizable();
+})

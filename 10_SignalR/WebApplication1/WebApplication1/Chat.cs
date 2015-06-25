18248c5dbdef2824553397e9a88b59f65e322989
Brock Allen <brockallen@gmail.com>
2015-06-25 18:35:09 -0400

    html5

---
+++ 10_SignalR/WebApplication1/WebApplication1/Chat.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Chat : Hub
+    {
+        public override System.Threading.Tasks.Task OnConnected()
+        {
+            //this.Context.ConnectionId;
+            // this.Context.User
+
+            this.Groups.Add(this.Context.ConnectionId, "Admin");
+
+            return base.OnConnected();
+        }
+
+        public override System.Threading.Tasks.Task OnReconnected()
+        {
+            return base.OnReconnected();
+        }
+
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void SendMessage(string message)
+        {
+            //this.Clients.Group("Admin").AdminSecretMessage();
+            //this.Clients.Clients()
+
+            // this.Context.ConnectionId;
+            this.Clients.All.GotMessage(new {
+                message,
+                time = DateTime.Now.ToString()
+            });
+        }
+    }
+}
---
+++ 10_SignalR/WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+
+            Task.Factory.StartNew(async () =>
+            {
+                while(true)
+                {
+                    await Task.Delay(2000);
+                    GlobalHost.ConnectionManager.GetHubContext<Chat>().Clients.All.GotMessage(new { message = "SPAM", time = DateTime.Now });
+                }
+            });
+
+        }
+    }
+}

42f427bda0aeed66629016b371e3f893f1abdc05
Brock Allen <brockallen@gmail.com>
2015-08-18 15:48:39 -0400

    angular modules, services, and filters

---
+++ extra_AngularServices/WebApplication1/WebApplication1/app.js
@@ -0,0 +1,60 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module("app", ["utils"]);
+
+app.config(function (FooProvider, $httpProvider) {
+    FooProvider.enableLogging = true;
+});
+
+app.run(function (Foo, $http) {
+
+});
+
+app.provider("Foo", function () {
+    var provider = {
+        $get: function(){
+            return {
+                getPeople: function () {
+                    if (provider.enableLogging) {
+                        // do some logging
+                    }
+                }
+            };
+        }
+    };
+    provider.enableLogging = false;
+    return provider;
+});
+
+
+
+app.service("PersonSvc2", function () {
+    this.getPeople = function () {
+
+    };
+});
+
+app.factory("PersonSvc", function ($http) {
+    var svc = {
+        getPeople: function () {
+
+        },
+        getPerson: function (id) {
+
+        }
+    };
+    return svc;
+});
+
+app.controller("PeopleCtrl", ["$scope", "$http", "PersonSvc",
+    function ($scope, $http, PersonSvc) {
+        $scope.people = PersonSvc.getPeople();
+    }]
+);
+
+function PersonCtrl($scope, PersonSvc) {
+    $scope.person = PersonSvc.getPerson(5);
+}
+PersonCtrl.$inject = ["$scope", "PersonSvc"];
+app.controller("PersonCtrl", PersonCtrl);
+
---
+++ extra_AngularServices/WebApplication1/WebApplication1/utils.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+
+var utils_mod = angular.module("utils", []);
+
+utils_mod.filter("maxlen", function () {
+    return function (val, len) {
+        if (val.length > len) {
+            val = val.substr(0, len);
+            val += "...";
+        }
+        return val;
+    };
+});
+

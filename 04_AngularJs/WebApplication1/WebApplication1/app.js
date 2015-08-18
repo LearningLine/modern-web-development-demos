dfb8d3fc46b51d42c39b9181046dcb1daf9554aa
Brock Allen <brockallen@gmail.com>
2015-08-18 15:48:18 -0400

    angular intro

---
+++ 04_AngularJs/WebApplication1/WebApplication1/app.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="angular.min.js" />
+
+var mod = angular.module("fcmaApp.Demo1", []);
+
+mod.controller("PersonCtrl", function ($scope, $rootScope) {
+    console.log("PersonCtrl was called!");
+    
+    $scope.person = {
+        name: "Brock", age: 34,
+        children: [
+            { name: 'Mira' },
+            { name: 'Alice' },
+            { name: 'Bob' },
+            { name: 'Tess' }
+        ]
+    };
+
+    $scope.removeKid = function (person, idx) {
+        person.children.splice(idx, 1);
+    }
+
+    $scope.save = function (person, acceptTerms) {
+        console.log("saving person!", person, acceptTerms);
+    }
+
+    $scope.isValid = function (person) {
+        return person.age >= 21 && person.name;
+    }
+});
+
+mod.controller("InnerCtrl", function ($scope) {
+
+});

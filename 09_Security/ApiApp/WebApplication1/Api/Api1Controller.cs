01075fcb837d6250b04b26051db7c6e0670f1797
Brock Allen <brockallen@gmail.com>
2015-08-20 15:04:55 -0400

    added cors

---
+++ 09_Security/ApiApp/WebApplication1/Api/Api1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApplication1.Api
+{
+    [EnableCors("http://localhost:52784", "Accept, Content-Type, Authorization", "*")]
+    public class Api1Controller : ApiController
+    {
+        [Route("api1")]
+        //[EnableCors("http://localhost:52784", "Accept, Content-Type, Authorization", "GET")]
+        public IHttpActionResult Get()
+        {
+            var cp = (ClaimsPrincipal)User;
+            var claims = cp.Claims.Select(x => x.Type + ":" + x.Value).ToArray();
+            return Ok(new { message = "it worked", claims=claims });
+        }
+    }
+}
---
+++ 09_Security/ApiApp/WebApplication1/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup))]
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.Map("/api", api =>
+            {
+                api.UseIdentityServerBearerTokenAuthentication(new IdentityServer3.AccessTokenValidation.IdentityServerBearerTokenAuthenticationOptions
+                {
+                    Authority = "https://localhost:44333/core",
+                    RequiredScopes = new string[] { "api1" }
+                });
+
+                var config = new HttpConfiguration();
+                config.EnableCors();
+                //config.EnableCors(new EnableCorsAttribute("http://a", "*", "*"));
+                config.Filters.Add(new AuthorizeAttribute());
+                config.MapHttpAttributeRoutes();
+                api.UseWebApi(config);
+            });
+        }
+    }
+}

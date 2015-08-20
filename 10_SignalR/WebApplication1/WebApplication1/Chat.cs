f2f4ce337b4267637e57e8cb4ee9362336a93272
Brock Allen <brockallen@gmail.com>
2015-08-20 15:05:01 -0400

    signalr

---
+++ 10_SignalR/WebApplication1/WebApplication1/Chat.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Chat : Hub
+    {
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void SendMessage(string msg)
+        {
+            //this.Context.User
+            this.Clients.All.GotMessage("You said: " + msg);
+        }
+    }
+}

c0d84ba995299d1ef396a1e74feed3b6ba61461d
Jason Diamond <jason@diamond.name>
2015-11-18 17:07:43 -0800

    web api

---
+++ 08-web-api/WebApplication3/WebApplication3/Controllers/PeopleController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    [RoutePrefix("api/people")]
+    public class PeopleController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult GetPeople()
+        {
+            return Ok(new[] {
+                new Person { Name = "Jason" },
+                new Person { Name = "Alice" },
+                new Person { Name = "Bob" },
+            });
+        }
+
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult CreatePerson()
+        {
+            return Ok();
+        }
+
+        [Route(":id")]
+        public IHttpActionResult GetPerson(int id)
+        {
+            return Ok(new Person { Name = "Jason" });
+        }
+    }
+}
---
+++ 08-web-api/WebApplication3/WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication3.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IEnumerable<Person> Get()
+        {
+            //return "Hello, Web API!";
+
+            return new[] {
+                new Person { Name = "Jason" },
+                new Person { Name = "Alice" },
+                new Person { Name = "Bob" },
+            };
+        }
+
+        public IHttpActionResult Post(Person newPerson)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+ 
+            return Ok();
+        }
+    }
+
+    public class Person
+    {
+        [Required]
+        [StringLength(1)]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(18, 99)]
+        public int Age { get; set; }
+    }
+}

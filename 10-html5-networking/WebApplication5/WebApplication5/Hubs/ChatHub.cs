03aad4a490c065f2336b8f14767f4f8a5cd0e36a
Jason Diamond <jason@diamond.name>
2015-11-19 15:34:00 -0800

    networking

---
+++ 10-html5-networking/WebApplication5/WebApplication5/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication5.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string msg)
+        {
+            Clients.Others.receiveMessage(msg);
+            //Clients.Caller.receiveMessage(msg);
+            //Clients.All.receiveMessage(msg);
+
+            // send to one person
+            //Clients.Caller("satoheusaoetuh").receiveMessage(msg);
+            //Context.ConnectionId
+
+            // send to a group of clients
+            //Clients.Group("groupName").receiveMessage(msg);
+        }
+
+        public void JoinRoom(string roomName)
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+        }
+    }
+}
